65d310d131e0a02a466adf7c00ef9efa64eebb89
Rahul Solunke <rahul.solunke123@gmail.com>
2021-01-22 00:28:47 +0530

    Add files via upload
    input from user in python

---
+++ input.py
@@ -0,0 +1,6 @@
+int_text = input("Give me an integer number: ")
+int_num = int(int_text)
+float_text = input("Give me a float  number: ")
+float_num = float(float_text)
+result = int_num * float_num
+print("Your result is: ", result)
