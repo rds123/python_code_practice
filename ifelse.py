a3bcbfd9296a6e87eb1dca89fccdc2459133f119
Rahul Solunke <rahul.solunke123@gmail.com>
2021-01-22 12:06:48 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,40 @@
+print('Enter your marks:')
+sub1 = int(input("First subject: "))
+sub2 = int(input("Second subject: "))
+sub3 = int(input("Third subject: "))
+sub4 = int(input("Fourth subject: "))
+sub5 = int(input("Fifth subject: "))
+
+avg = (sub1 + sub2 + sub3 + sub4 + sub5) / 5
+
+if avg >= 90:
+    print("Grade: A")
+elif avg >= 80:
+    print("Grade: B")
+elif avg >= 70:
+    print("Grade: C")
+elif avg >= 60:
+    print("Grade: D")
+else:
+    print("Grade: F")
+
+    C:\Users\Rahul\PycharmProjects\pythonProject\venv\Scripts\python.exe
+    C: / Users / Rahul / PycharmProjects / pythonProject / ifelse.py
+    Enter
+    your
+    marks:
+    First
+    subject: 34
+    Second
+    subject: 56
+    Third
+    subject: 78
+    Fourth
+    subject: 98
+    Fifth
+    subject: 67
+    Grade: D
+
+    Process
+    finished
+    with exit code 0
